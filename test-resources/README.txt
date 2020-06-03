2e11744ea726aa4a3aa5c17c9ebc7b7f5908d5b8
arjunvnair <arjunvnair@hotmail.com>
2020-06-03 00:07:19 -0500

    Implement scrape_variable and VariableListener
    Passes all unit tests.

--- test-resources/README.txt
+++ test-resources/README.txt
@@ -1,3 +1,3 @@
-The following is a repository of algorithms that is unlicensed, and free for anyone to use. We are using these source files for the purposes of testing our variable scraper.
+The following is derived from a repository of algorithms that is unlicensed and free for anyone to use. We are using these source files for the purposes of testing our variable scraper.
 
 Link to Repository: https://github.com/indy256/codelibrary
--- vivid_variables.py
+++ vivid_variables.py
@@ -3,34 +3,45 @@ from antlr4 import *
 sys.path.append('antlr/gen/java')
 from JavaLexer import JavaLexer
 from JavaParser import JavaParser
+from JavaParserListener import JavaParserListener
+
+class VariableListener(JavaParserListener):  
+    """Custom listener for scraping variable names (does not include those declared in for control statements)."""
+
+    
+    def __init__(self):
+        super(VariableListener, self).__init__() # Call base constructor
+        self.in_for_control = False # Whether the listener is currently walking through a for control statement
+        self.variable_list = [] # List of variable names not declared in for control statements
+
+
+    def enterForControl(self, ctx):         
+        self.in_for_control = True
+
+    def exitForControl(self, ctx):         
+        self.in_for_control = False
+
+    def enterVariableDeclarator(self, ctx):
+        if not self.in_for_control:
+            self.variable_list.append(ctx.getChild(0).getText())
+
  
-def main(argv):
-    input_stream = FileStream(argv[1])
+def generate_parse_tree(source):
+    """Generates an ANTLR parse tree from a piece of source code."""
+
+    input_stream = FileStream(source)
     lexer = JavaLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = JavaParser(stream)
-    tree = parser.statement()
-    print(tree.getChild(0).getText())
- 
-if __name__ == '__main__':
-    main(sys.argv)
+    tree = parser.compilationUnit()
+    return tree
 
-"""
-code = open('sample.java', 'r').read()
-codeStream = InputStream(code)
-lexer = JavaLexer(codeStream)
+def scrape_variables(source):
+    """Gathers all the names of the variables instantiated, except for those in for control statements."""
 
-# First lexing way
-tokensStream = CommonTokenStream(lexer)
-parser = JavaParser(tokensStream)
+    tree = generate_parse_tree(source)
+    listener = VariableListener()
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    return listener.variable_list
 
-# Second lexing way
-'''tokens = lexer.getAllTokens()
-tokensSource = ListTokenSource(tokens)
-tokensStream = CommonTokenStream(tokensSource)
-parser = JavaParser(tokensStream)'''
-
-tree = parser.compilationUnit()
-print("Tree " + tree.toStringTree(recog=parser))
-
-"""
